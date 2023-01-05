bed9363793701bf715779fb4fd6021546c5dab71
fjolivo <fjolivo@gmail.com>
2023-01-04 23:52:34 -0400

    About section done

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-import { AppWrap } from '../../wrapper';
+// import { AppWrap } from '../../wrapper';
 import { images } from '../../constants';
 import './Header.scss';
 
@@ -69,4 +69,5 @@ const Header = () => (
 );
 
 
-export default AppWrap(Header, 'home');t
+// export default AppWrap(Header, 'home');
+export default Header;
