13294fbcccfbbd4814812a23d7dfd3d329cc94aa
fjolivo <fjolivo@gmail.com>
2023-01-16 19:55:57 -0400

    Upgraded to React 18
    Now fixing bugs with styling

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -1,7 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// // import ReactDOM from 'react-dom';
 
-import App from './App';
-import './index.css';
+// import { ReactDOM } from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import App from './App';
+// import './index.css';
+
+// // ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.createRoot(<App />, document.getElementById('root'))
+
+
+
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+
+// const root = ReactDOM.createRoot(<App />, document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = (<App />, document.getElementById('root'));
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
