c66a57d8acc5f8db08969cf5cda8dc29caea822b
fjolivo <fjolivo@gmail.com>
2023-02-06 22:36:39 -0400

    Now beautiful and responsive in 4k

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -8,9 +8,9 @@ const NavigationDots = ({ active }) => (
         href={`#${item}`}
         key={item + index}
         className="app__navigation-dot"
-        style={active === item ? { backgroundColor: '#313BAC' } : {}}
+        // style={active === item ? { backgroundColor: '#313BAC' } : {}}
       >
-        {null}
+        {/* {null} */}
       </a>
     ))}
   </div>
